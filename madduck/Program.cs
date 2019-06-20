508b7c92b03732f114408ae5161c043d9f78eb3f
connecttoconnecticut96 <boban96minic@gmail.com>
2019-06-20 22:13:15 +0200

    Project init

---
+++ madduck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace madduck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
